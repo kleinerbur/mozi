a32181d61c0234d7f05549bdc578b8353a98df44
kleinerbur <burbence@gmail.com>
2023-09-10 18:43:05 +0200

    Restructured assistant data model

--- server/src/Assistant.js
+++ server/src/Assistant.js
@@ -1,32 +1,31 @@
-var ArtMozi = require('./cinema/ArtMozi');
-var Corvin = require('./cinema/Corvin');
+var ArtMozi    = require('./cinema/ArtMozi');
+var Corvin     = require('./cinema/Corvin');
 var CinemaCity = require('./cinema/CinemaCity');
 
 class Assistant {
     constructor() {
-        this.films = [];
+        this.films    = [];
         this.showings = [];
-        this.currentDate = new Date();
-
-        this.artmozi = new ArtMozi();
-        this.corvin  = new Corvin();
-        this.cinemacity = new CinemaCity();
+        this.cinemas  = [
+            new ArtMozi(),
+            new Corvin(),
+            new CinemaCity()
+        ]
     }
 
     async pull(date) {
-        this.showings = [].concat(...await Promise.all([
-            this.artmozi.pull(date),
-            this.corvin.pull(date),
-            this.cinemacity.pull(date)
-        ])).sort((a,b) => a.title.localeCompare(b.title)
-                       || new Date(a.startTime) - new Date(b.startTime)
-                       || a.cinema.localeCompare(b.cinema));
-        this.films =
-            [...new Set(
-                this.showings
-                    .filter(showing => showing !== null)
-                    .map(showing => showing.title)
-            )].sort((a,b) => a.localeCompare(b));
+        this.showings = [].concat(...await Promise.all(
+            this.cinemas.map(cinema => cinema.pull(date))
+        )).sort((a,b) => 
+            a.title.localeCompare(b.title)
+            || new Date(a.startTime) - new Date(b.startTime)
+            || a.cinema.localeCompare(b.cinema)
+        );
+        this.films = [...new Set(
+            this.showings
+                .filter(showing => showing !== null)
+                .map(showing => showing.title)
+        )].sort((a,b) => a.localeCompare(b));
     }
 }
 
